165101cb6c9be55c3440ebfc7fc03cda01872569
varnika97 <vannuvarnika8@gmail.com>
2025-12-19 13:54:23 +0530

    only-todolist

---
+++ First-React-app/src/components/OnlyTodolist.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react"
+function OnlyTodolist(){
+    var [todos, setTodo] =useState(['books','Games',"Movies",'Travel'])
+    function addNewtodo(){
+        var ntodo = document.getElementById('d1').value;
+        setTodo([...todos, ntodo])
+    }
+    return(
+        <div className="border border-2 border-success m-2 p-2">
+           <h1>Todolist</h1>
+           <input type="text" id="d1" />
+        <button onClick={()=>{addNewtodo()}}>Add Todo</button>
+           <ul className="list-unstyled">{
+              todos.map((t)=>{
+                return <li className=" border border-2 m-2 p-2">{t}</li>
+              })
+            }</ul>
+        </div>
+    )
+}
+export default OnlyTodolist
